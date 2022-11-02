89da162faf4bb9db4a444b0bc7bcd667f687c47a
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-11-02 07:36:15 -0700

    made changes to date on footer

--- src/secondbox/Footer.jsx
+++ src/secondbox/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-
+const date= new Date().getFullYear();
 
 function Footer() {
   return (
@@ -12,7 +12,7 @@ function Footer() {
       <a className="links" href="https://stackoverflow.com/users/5482255/peter-moses">stackoverflow</a>
       <a className="links" href="https://web.facebook.com/mohemos?_rdc=1&_rdr">Facebook</a>
       <a className="links" href="https://twitter.com/mohemos">Twi</a>
-      <p className="para para2"> © Moses Hennuho Peter, 2018</p>
+      <p className="para para2"> © Moses Hennuho Peter, {date}</p>
       
     </div>
   );
